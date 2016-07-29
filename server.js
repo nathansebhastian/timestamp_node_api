c849fb48b797f1b16f09685612f2d591290af954
Yonathan Sebhastian <yonathansebhastian@gmail.com>
2016-07-29 14:12:45 +0000

    finishing touch

--- server.js
+++ server.js
@@ -1,10 +1,28 @@
-var express = require('express');
+var express = require("express");
+var path = require("path");
+var moment = require("moment");
 var app = express();
+var result = {unix: null, natural: null};
+
+app.get('/:query', function (req, res) {
+  var query = new Date(req.params.query);
+  
+  if(Object.prototype.toString.call(query) === "[object Date]"){
+    if (!isNaN(query.getTime())) {
+			result.unix = moment(query).format("X"); 
+			result.natural = moment(query).format("MMMM Do, YYYY");
+		// 	result.natural = months[query.getMonth()] + ' ' + query.getDate() + ', ' + query.getFullYear();
+		}
+  }
+  
+  res.writeHead(200, { 'Content-Type': 'application/json'});
+	res.end(JSON.stringify(result));
+});
+
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.sendFile(path.join(__dirname + '/public/index.html'));
 });
 
-app.listen(8080, function () {
-  console.log('Example app listening on port 8080!');
-});
+
+app.listen(process.env.PORT || 8080 || 5000);
